659f5ef4380afb6696fa7bda4f04ff7041e5f2f5
JoelHauser <joel.hauser@gmail.com>
2022-05-12 01:09:52 -0400

    big ol update

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/', (req, res) => {
+    console.log('--------------');
+    Post.findAll({
+        attributes: ['id', 'title', 'content', 'created_at'],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']   
+                }
+            }
+        ]
+    })
+    .then(dbPostData => res.json(dbPostData.reverse()))
+    .catch(err => {
+        console.log(err);
+        res.json(500).json(err);
+    })
+})
